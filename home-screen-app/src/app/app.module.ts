cfe57d9abeadd7288dcf38f6aaa718ed324e9bd5
ishitasaggi <ishita.saggi@somrubioscience.com>
2025-01-28 07:26:20 -0400

    Update for Home page

---
+++ home-screen-app/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [], // No need to declare HomeComponent here
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
