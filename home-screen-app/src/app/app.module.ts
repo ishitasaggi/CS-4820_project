b7460ecf11d2e9d9c359380435cf16459603531c
ishitasaggi <ishita.saggi@somrubioscience.com>
2025-01-30 13:43:38 -0400

    Update according to Predictive dose model

--- home-screen-app/src/app/app.module.ts
+++ home-screen-app/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [], // No need to declare HomeComponent here
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: []
 })
